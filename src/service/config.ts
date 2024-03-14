2026e8587bd06d0e5eba7afe0f98469a04bd1c63
pengzhifang <pengzhifang@xdf.cn>
2024-03-14 22:46:06 +0800

    feat: 接口配置

--- src/service/config.ts
+++ src/service/config.ts
@@ -117,6 +117,94 @@ export const publishTreatmentPlantApi = (data): Promise<any> => {
 }
 
 /**
+ * 配置管理-物业公司配置-列表
+ * @param data 
+ * @returns 
+ */
+export const propertyListApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/property/v1/page-list`, data);
+}
+
+/**
+ * 配置管理-物业公司配置-新建
+ * @param data 
+ * @returns 
+ */
+export const addPropertyApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/property/v1/insert`, data,);
+}
+
+/**
+ * 配置管理-物业公司配置-修改
+ * @param data 
+ * @returns 
+ */
+export const updatePropertyApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/property/v1/update`, data,);
+}
+
+/**
+ * 配置管理-物业公司配置-删除
+ * @param data 
+ * @returns 
+ */
+export const deletePropertyApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/property/v1/delete`, data,);
+}
+
+/* 配置管理-物业公司配置-开通/下线
+* @param data 
+* @returns 
+*/
+export const publishPropertyApi = (data): Promise<any> => {
+ return axiosPost(`/user-api/property/v1/publish`, data,);
+}
+
+/**
+ * 配置管理-项目（小区）配置-列表
+ * @param data 
+ * @returns 
+ */
+export const residentialListApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/residential/v1/page-list`, data);
+}
+
+/**
+ * 配置管理-项目（小区）配置-新建
+ * @param data 
+ * @returns 
+ */
+export const addResidentialApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/residential/v1/insert`, data,);
+}
+
+/**
+ * 配置管理-项目（小区）配置-修改
+ * @param data 
+ * @returns 
+ */
+export const updateResidentialApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/residential/v1/update`, data,);
+}
+
+/**
+ * 配置管理-项目（小区）配置-删除
+ * @param data 
+ * @returns 
+ */
+export const deleteResidentialApi = (data): Promise<any> => {
+  return axiosPost(`/user-api/residential/v1/delete`, data,);
+}
+
+/* 配置管理-项目（小区）配置-开通/下线
+* @param data 
+* @returns 
+*/
+export const publishResidentialApi = (data): Promise<any> => {
+ return axiosPost(`/user-api/residential/v1/publish`, data,);
+}
+
+/**
  * 获取中国所有省市区
  * @param data 
  * @returns 
